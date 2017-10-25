0db6ba36c523e28e32cde5612b38a7d19104a66f
Joshua George <josh@ihateonions.uk>
2017-10-25 10:20:54 +0100

    Initial Upload

---
+++ js/uuid.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+document.body.onkeyup = function(e) {
+  if( e.keyCode == 32 ) {
+    UUID();
+  }
+}
+UUID();
+});
+
+
+var UUID = function () {
+
+	function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+    document.getElementById('uuid').innerHTML = generateUUID();
+	document.getElementById('uuid').innerHTML = generateUUID();
+
+}
